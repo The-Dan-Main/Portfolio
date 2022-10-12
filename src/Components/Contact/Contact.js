acf1226364a8a4a611b1d684c04a67fba6f3540d
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-10-13 00:35:13 +0200

    ...

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
 
     useEffect(() => {
         window.scrollTo(0, 0)
-    },[])
+    }, [])
 
     const reset = (e) => {
         e.preventDefault()
@@ -24,21 +24,20 @@ export default function Contact() {
 
 
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     const data = { name: name, email: email, type: type, message: message }
-    //     console.log("submitted")
-    //     console.log(data)
-    //     fetch("/contact", {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //         body: data
-    //       })
-    //         .then(() => alert("Success!"))
-    //         .catch(error => alert(error));
-    
-    //     //   e.preventDefault();
-    // }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const data = { name: name, email: email, type: type, message: message }
+        console.log("submitted")
+        console.log(data)
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encodeURI({ "form-name": "contact", ...data })
+          })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+          e.preventDefault();
+    }
 
     return (
         <div className='contact-container'>
@@ -50,14 +49,15 @@ export default function Contact() {
             <h3>Let me know what you need:</h3>
 
             <form
-                name="contact"
+                name="contact-form"
                 method="POST"
                 data-netlify="true"
                 className="form-container"
-                // action="/"
-                // onSubmit={(e) => handleSubmit(e)}
+                action="/"
+                onSubmit={handleSubmit}
                 netlify-honeypot="bot-field"
             >
+                <input type="hidden" name="form-name" value="contact-form" />
                 <p className="hidden">
                     <label>
                         Dont fill this out if youre human: <input name="bot-field" />
@@ -73,7 +73,7 @@ export default function Contact() {
                 </p>
                 <p>
                     <label>Type of Website: </label>
-                    <select required name="inquiery Type[]" onChange={(e) => setType(e.target.value)} value={type}>
+                    <select required name="inquiery Type" onChange={(e) => setType(e.target.value)} value={type}>
                         <option value="Blog">Blog</option>
                         <option value="Info-Site">Info-Page</option>
                         <option value="Company-Site">Company-Page</option>
@@ -82,7 +82,9 @@ export default function Contact() {
                 </p>
                 <p>
                     <label>Message: </label>
-                    <textarea required name="Message from costumer"
+                    <textarea
+                        required
+                        name="Message from costumer"
                         placeholder="describe your project and give me an idea what functions it will need..."
                         onChange={(e) => setMessage(e.target.value)}
                         value={message}
