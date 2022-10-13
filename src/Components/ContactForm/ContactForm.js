31aa328f2c1e276857d874f0bbb3afac5aace2aa
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-10-13 14:36:09 +0200

    fixed submitting form finally

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,63 +1,97 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./ContactForm.css"
 
 const encode = (data) => {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
         .join("&");
-  }
+}
 
-  class ContactForm extends React.Component {
+class ContactForm extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = { name: "", email: "", message: "", age: ""};
+        super(props);
+        this.state = { name: "", email: "", message: "", age: "", type: "Blog" };
     }
 
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
     /* Here’s the juicy bit for posting the form submission */
 
-    handleSubmit = e => {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
+    reset = (e) => {
+        e.preventDefault()
+        this.setState({ name: "", email: "", message: "", age: "", type: "Blog" })
+    }
 
-      e.preventDefault();
+    handleSubmit = e => {
+        console.log(this.state)
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => console.log("Successfully submitted!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
     };
 
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
+    // handleSelectChange = e => {
+    //     console.log(e.target.name)
+    //     this.setState({ [e.target.name]: e.target.value })};
 
     render() {
-      const { name, email, message, age } = this.state;
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Age: <textarea name="age" value={age} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      );
-    }
-  }
+        const { name, email, message, type } = this.state;
+        return (
+            <div className='contact-container'>
+                <Link to="/" id="backToPortfolioContainer">
+                    <h3 id="backToPortfolioTitle">Back to Portfolio</h3>
+                    <img src={require("../Content/images/—Pngtree—hand painted white arrows_6020093.png")} alt="back to Portfolio" id="backToPortfolio"></img>
+                </Link>
+                <h3>I would love to build something for you!</h3>
+                <h3>Let me know what you need:</h3>
 
-  export default ContactForm
+                <form onSubmit={this.handleSubmit} className="form-container" >
+                    <p>
+                        <label>
+                            Your Name:<br></br>
+                            <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your Email:<br></br>
+                            <input type="email" name="email" value={email} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>Type of Website:<br></br>
+                            <select required name="type" onChange={this.handleChange} value={type}>
+                                <option value="Inital">Please choose...</option>
+                                <option value="Blog">Blog</option>
+                                <option value="Info-Site">Info-Page</option>
+                                <option value="Company-Site">Company-Page</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </label>
+                    </p>
+                    <p>
+                        <label>Message:<br></br>
+                            <textarea name="message" value={message} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <label id="ready-title">Ready to send?</label>
+                    <p id="button-section">
+                        <button type="submit" >Send</button>
+                        <button type="reset" onClick={(e) => this.reset(e)}>Reset</button>
+                    </p>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default ContactForm
