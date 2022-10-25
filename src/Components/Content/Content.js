4252f4dae5c1bc2c123aa3f020a418af08e0c772
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-10-25 12:32:15 +0200

    got homepage working incl. navbar and pgaelinks

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,13 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import './Content.css'
 
 export default function Content() {
+    const [isOpen, setIsOpen] = useState(0)
 
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
+
+
+    const toggleMenu = () => {
+        console.log("menu toggled")
+        // setIsOpen(1)
+        console.log(isOpen)
+        isOpen === 1 ? setIsOpen(0) : setIsOpen(1)
+        // isOpen === 1 ? setIsOpen(0) : setIsOpen(1)
+    }
 
     return (
         <div className="content-wrap">
@@ -34,21 +44,29 @@ export default function Content() {
                 <span></span>
             </div>
             <div className='content-container'>
-                <Navbar />
+                {isOpen === 1 && <Navbar />}
+                <label htmlFor="check" className='navbar-menu' >
+                    <input type="checkbox" id="check" onClick={()=>toggleMenu()}/>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
 
                 <div className="content-introduction">
                     <h2 className="content-intro-title">Dan Weber</h2>
                     <h2 className="content-intro-position">Junior Front-End Developer</h2>
                     <p className="content-intro-desc">
-                        Hello there! After a adventerous carreer in hospitality, I found a new and ravishing passion! <br />
+                        Hello there! After an adventerous carreer in hospitality, I found a new and ravishing passion!
                         I am a young developer from downtown Zurich, Switzerland and love to build front-end / fullstack projects.
                     </p>
                 </div>
 
 
-
-
-                <Link to="/contact" className="contact-title">click here to get to my contact form</Link>
+                <div className="PageLink-Container">
+                    <Link to="/projects" className="content-PageLinks"><span data-content="PROJECTS" aria-hidden="true"></span>PROJECTS</Link>
+                    <Link to="/contact" className="content-PageLinks"><span data-content="CONTACT" aria-hidden="true"></span>CONTACT</Link>
+                    <Link to="/about" className="content-PageLinks"><span data-content="ABOUT" aria-hidden="true"></span>ABOUT</Link>
+                </div>
 
             </div>
         </div>
